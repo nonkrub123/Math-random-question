052633b5fa0d4c9843293f30f9cc64fa735c0f44
nonkrub123 <Tichanon.k@gmail.com>
2025-01-14 18:52:01 +0700

    test

--- math-quiz/vite.config.ts
+++ math-quiz/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/Math-random-question/',
+  plugins: [react()],
+  base: '/Math-random-question/', // Add your repo name here
 });
